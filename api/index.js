2bf95cb60d878989ad523369cc676bcf50710751
sajeeb-lab <sajeeb@appsus.io>
2026-07-22 15:43:09 +0600

    Add PATCH method to CORS configuration

--- api/index.js
+++ api/index.js
@@ -442,7 +442,7 @@ if (missingEnv.length) {
 const app = express();
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.options('*', cors());
